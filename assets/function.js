3f6e058c1131bc33f5bde3a6f581bb4ee015e54e
Inbary-s <inbarsim6@gmail.com>
2019-12-17 17:52:28 -0700

    fixing style issues, adding bubblies

--- assets/function.js
+++ assets/function.js
@@ -1,9 +1,20 @@
+
 M.AutoInit();
 $(document).ready(function(){
   $('.scrollspy').scrollSpy();
+  
 });
 $(".images").mouseover(function () {
     $(this).attr('src', $(this).data("hover"));
   }).mouseout(function () {
     $(this).attr('src', $(this).data("src"));
   });
+  document.addEventListener('scroll', function(){
+    var scrolly = window.scrollY;
+    if(!scrolly){
+      $('.navbar').css('visibility', 'hidden')
+    }else{
+      console.log('scrolling')
+      $('.navbar').css('visibility', 'visible')
+    }
+  })
